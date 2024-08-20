aa885ebab607c55f6c5e7ba646f4d060b67e2cb9
Infinite-XD <mustafakhattak360@icloud.com>
2024-08-20 11:39:09 +0100

    Create Board Class Simulating Game Board create seperate functions for checking each win possibility

---
+++ lib/board.rb
@@ -0,0 +1,46 @@
+# Board Class, generates 2D array simulating a tic-tac-toe board.
+class Board
+  attr_accessor :board
+
+  def initialize
+    @board = Board.create_board
+  end
+
+  def self.create_board
+    Array.new(3) { Array.new(3, " ") }
+  end
+
+  def check_victory(arr)
+    true if arr.all? { |item| item == "X" } || arr.all? { |item| item == "Y" }
+  end
+
+  def check_horizontal(arr)
+    arr.each do |item|
+      return true if item.all? { |inner_item| inner_item == "X" } || item.all? { |inner_item| inner_item == "Y" }
+    end
+    false
+  end
+
+  def check_vertical(arr)
+    temp = []
+    3.times do |num|
+      3.times do |number|
+        temp.push(arr[number][num])
+      end
+      p temp
+      return true if check_victory(temp) == true
+
+      temp = []
+    end
+    false
+  end
+
+  def check_diagnol(arr)
+    temp = []
+    3.times do |number|
+      temp.push(arr[number][number])
+    end
+
+    false
+  end
+end
